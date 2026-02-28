e4ea12853cb73086b0fbb42d36a4b8364214772c
declan <dfruzyna@iastate.edu>
2026-02-28 16:15:58 -0600

    implemented the test cases using the zombie method

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ using namespace std;
 int main() {
   // Your driver program goes here
   vector<vector<int>> rets;
-  rets.push_back(prime(73));
+  rets.push_back(prime(-1));
+  rets.push_back(prime(0));
+  rets.push_back(prime(1));
   rets.push_back(prime(20));
   rets.push_back(prime(2048));
   rets.push_back(prime(126));
--- tests/prime_test.cpp
+++ tests/prime_test.cpp
@@ -4,7 +4,30 @@
 #include <catch2/generators/catch_generators_range.hpp>
 
 #include "../src/prime.hpp"
+#include <vector>
 
-TEST_CASE( "#prime" ) {
-    REQUIRE( prime() == "You can delete this function and replace it with your own!" );
+TEST_CASE( "#zero" ) {
+    std::vector<int> test = {};
+    REQUIRE( prime(0) == test);
 }
+TEST_CASE( "#1" ) {
+    std::vector<int> test = {};
+    REQUIRE( prime(1) == test);
+}
+TEST_CASE( "#126 - large non prime - many" ) {
+    std::vector<int> test = {2, 3, 3, 7};
+    REQUIRE( prime(126) == test);
+}
+TEST_CASE( "#2 - bound(>1)" ) {
+    std::vector<int> test = {2};
+    REQUIRE( prime(2) == test);
+}
+TEST_CASE( "#20 - sample" ) {
+    std::vector<int> test = {2, 2, 5};
+    REQUIRE( prime(20) == test);
+}
+TEST_CASE( "#67 - large prime" ) {
+    std::vector<int> test = {67};
+    REQUIRE( prime(67) == test);
+}
+
